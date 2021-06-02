fa071cfa8a816a0a1f0f90c568c1ba1eada38505
Rinat <lich666black@gmail.com>
2021-06-02 17:34:26 +0500

    fix(added styled component): added styled component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,39 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styled from 'styled-components';
+import React from 'react';
+import { NextPage } from 'next';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Nav = styled.nav`
+  max-width: 56em;
+  margin: 0 auto;
+  padding: 0.4em;
+`;
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const Ul = styled.ul`
+  display: flex;
+  padding: 0;
+`;
 
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+const Li = styled.li`
+  display: block;
+  padding: 0.4em;
+  color: red;
+`;
 
-        <p className={styles.description}>This is not an official starter!</p>
+export const MainPage: NextPage<PageProp> = () => (
+  <Nav>
+    <Ul>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+      <Li>1dasdsa</Li>
+    </Ul>
+  </Nav>
+);
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{` `}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  );
-}
+export default MainPage;
