6176a7d8c8d64b78133cce247c69ba1c2b2ca566
Rinat <lich666black@gmail.com>
2021-06-10 20:02:49 +0500

    added form for buy page

--- src/pages/buy.tsx
+++ src/pages/buy.tsx
@@ -1,30 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
 import { NextPage } from 'next';
-import { BrowserView, MobileView } from 'react-device-detect';
 
 import { Navbar } from 'components/nav-bar';
+import { Text } from 'components/text';
+
+import { Colors } from '@/config/colors';
+import { StyleFonts } from '@/config/fonts';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 40vh;
+  justify-content: space-between;
+`;
+const Form = styled.form`
+  background: ${Colors.Secondary};
+  padding: 36px;
+  border-radius: 16px;
+  max-width: 599px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 export const BuyPage: NextPage = () => (
-  <>
-    <BrowserView>
-      <Container>
-        <Navbar />
-        <h1> This is rendered only in browser </h1>
-      </Container>
-    </BrowserView>
-    <MobileView>
-      <Container>
-        <h1> This is rendered only in mobile </h1>
-      </Container>
-    </MobileView>
-  </>
+  <Container>
+    <Navbar />
+    <Form>
+      <TitleWrapper>
+        <Text
+          fontVariant={StyleFonts.FiraSansSemiBold}
+          size="24px"
+          css="margin-right: 10px;"
+        >
+          Buy eggs
+        </Text>
+        <img
+          src="/icons/egg.svg"
+          alt="egg"
+        />
+      </TitleWrapper>
+    </Form>
+  </Container>
 );
 
 export default BuyPage;
