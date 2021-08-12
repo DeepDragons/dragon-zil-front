7b8d996495516804995e739c6b031564d1c9527a
Rinat <lich666black@gmail.com>
2021-08-12 13:03:00 +0500

    removed modal and fixed connect button

--- src/components/connect-zilpay.tsx
+++ src/components/connect-zilpay.tsx
@@ -4,7 +4,6 @@ import { useStore } from 'effector-react';
 import styled from 'styled-components';
 
 import Loader from "react-loader-spinner";
-import { Button } from 'components/button';
 
 import { StyleFonts } from 'config/fonts';
 import { Colors } from 'config/colors';
@@ -12,15 +11,28 @@ import { ZilPayBase } from 'mixin/zilpay-base';
 import { trim } from 'lib/trim';
 import { $wallet, updateAddress } from 'store/wallet';
 
-const Container = styled.div`
+const ConnectButton = styled.div`
   cursor: pointer;
   color: ${Colors.White};
   font-family: ${StyleFonts.FiraSansSemiBold};
+
+  padding: 10px 22px;
+  border-radius: 16px;
+  border: 1px solid ${Colors.Darker};
+  background: ${Colors.Darker};
+  user-select: none;
+  min-width: 100px;
+  text-align: center;
+
+  :hover {
+    border: 1px solid ${Colors.Muted};
+  }
 `;
 
 export const ConnectZIlPay: React.FC = () => {
   const address = useStore($wallet);
-  const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [isOpen, setIsOpen] = React.useState(true);
 
   const handleConnect = React.useCallback(async() => {
     setLoading(true);
@@ -47,32 +59,37 @@ export const ConnectZIlPay: React.FC = () => {
         .zilpay
         .then((zp) => {
           if (zp.wallet.defaultAccount) {
-            updateAddress(zp.wallet.defaultAccount)
+            updateAddress(zp.wallet.defaultAccount);
+            setLoading(false);
           }
-        });
+        })
+        .catch(() => setLoading(false));
     } catch (err) {
+      setLoading(false);
       console.error(err);
     }
   }, []);
 
   if (address) {
     return (
-      <Container>
+      <ConnectButton>
         {trim(address.bech32)}
-      </Container>
+      </ConnectButton>
     );
   }
 
   return (
-    <Button onClick={handleConnect}>
-      {loading ? (
-        <Loader
-          type="ThreeDots"
-          color="#fff"
-          height={10}
-          width={20}
-        />
-      ) : 'Connect'}
-    </Button>
+    <>
+      <ConnectButton onClick={handleConnect}>
+        {loading ? (
+          <Loader
+            type="ThreeDots"
+            color="#fff"
+            height={10}
+            width={20}
+          />
+        ) : 'Connect'}
+      </ConnectButton>
+    </>
   );
 };
