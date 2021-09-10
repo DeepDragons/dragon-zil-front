cf790862b775ecf0e067f2e6d809cb41f292a99f
Rinat <lich666black@gmail.com>
2021-09-10 20:43:03 +0300

    fixed host

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -38,7 +38,7 @@ export interface PaginationObject {
 }
 
 export class DragonAPI {
-  private _host = 'http://127.0.0.1:8083';
+  private _host = 'http://127.0.0.1:80';
   private _api = 'api/v1';
 
   public async getDragons(owner: string, limit = 6, offset = 0) {
