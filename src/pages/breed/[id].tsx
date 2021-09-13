02368c1aa692da8e5e6d3dec4890c238e3177795
Rinat <lich666black@gmail.com>
2021-09-13 18:43:36 +0300

    fixed breeding

--- src/pages/breed/[id].tsx
+++ src/pages/breed/[id].tsx
@@ -64,7 +64,7 @@ export const BreedStart: NextPage<Prop> = ({ lover }) => {
       //
     }
     setLoading(false);
-  }, [lover, myDragon]);
+  }, [lover, myDragon, needApprove]);
 
   const rarityLover = React.useMemo(() => {
     if (!lover) {
