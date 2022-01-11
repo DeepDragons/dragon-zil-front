ca915535d8f92f2b192d3fb74cdf278e465a690f
Rinat <lich666black@gmail.com>
2022-01-11 13:31:40 +0300

    fixed fight place

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -15,7 +15,7 @@
 export enum Contracts {
   Distributer = `0x066b88d3411c68cb56219e748ae895e1734c0f51`,
   Main = `0xb4d83becb950c096b001a3d1c7abb10f571ae75f`,
-  FightPlace = `0x2c72d59ffda03570f94cfc8c3e562a211dfd2fe7`,
+  FightPlace = `0xf0a3fbcfa48e4c796daafbb9c6341d68ff326b64`,
   GenLab = `0x8a30485597ebcc1be49e75261de83d30e4a9d1e7`,
   ZIlPay = `0xfbd07e692543d3064b9cf570b27faabfd7948da4`,
   Breed = `0xade7886ec4a36cb0a7de2f5d18cc7bdae12e3650`,
